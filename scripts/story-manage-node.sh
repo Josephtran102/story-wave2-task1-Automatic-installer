aaa1e4532f52956ac1afaf137e724ad863af5249
Joseph Tran <115780760+Josephtran102@users.noreply.github.com>
2024-10-07 13:19:01 +0700

    Update story-manage-node.sh

--- scripts/story-manage-node.sh
+++ scripts/story-manage-node.sh
@@ -81,11 +81,30 @@ change_port() {
     read -n 1 -s -r -p "Press any key to continue"
 }
 
+# Function to export keys
+export_keys() {
+    clear
+    print_color "blue" "=== Export Keys | J•Node | www.josephtran.xyz ==="
+    
+    print_color "yellow" "Loading bash profile..."
+    source $HOME/.bash_profile
+    check_status "Bash profile loaded"
+
+    print_color "yellow" "Exporting keys..."
+    story validator export --export-evm-key
+    check_status "Keys exported"
+
+    print_color "blue" "Keys have been exported. Please check the output above for the location of the exported keys."
+    read -n 1 -s -r -p "Press any key to continue"
+}
+
+
 # Function to manage node (stop/start) and check versions
 manage_node() {
     local options=(
         "Check Versions"
         "Change port for node"
+        "Export Keys"
         "Stop Story" 
         "Stop Story-Geth" 
         "Stop Both" 
@@ -136,36 +155,37 @@ manage_node() {
     case $current in
         0) check_versions ;;
         1) change_port ;;
-        2) 
+        2) export_keys ;;
+        3) 
             sudo systemctl stop story
             check_status "Story stopped"
             ;;
-        3) 
+        4) 
             sudo systemctl stop story-geth
             check_status "Story-Geth stopped"
             ;;
-        4) 
+        5) 
             sudo systemctl stop story
             sudo systemctl stop story-geth
             check_status "Both services stopped"
             ;;
-        5) 
+        6) 
             sudo systemctl start story
             check_status "Story started"
             ;;
-        6) 
+        7) 
             sudo systemctl start story-geth
             check_status "Story-Geth started"
             ;;
-        7) 
+        8) 
             sudo systemctl start story
             sudo systemctl start story-geth
             check_status "Both services started"
             ;;
-        8) return ;;
+        9) return ;;
         *) print_color "red" "Invalid choice" ;;
     esac
-    [[ $current != 8 ]] && read -n 1 -s -r -p "Press any key to continue"
+    [[ $current != 9 ]] && read -n 1 -s -r -p "Press any key to continue"
 }
 
 # Main execution
