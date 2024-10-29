f12017c21ef7110bb67baba4477dc4aede227ed7
Joseph Tran <115780760+Josephtran102@users.noreply.github.com>
2024-10-29 11:54:58 +0700

    Update story-node-installer.sh

--- scripts/story-node-installer.sh
+++ scripts/story-node-installer.sh
@@ -39,15 +39,13 @@ install_dependencies() {
 # Download and install Story-Geth
 install_story_geth() {
     print_color "blue" "Downloading and installing Story-Geth..."
-    wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/geth-public/geth-linux-amd64-0.9.2-ea9f0d2.tar.gz
-    tar -xzvf geth-linux-amd64-0.9.2-ea9f0d2.tar.gz
-    rm geth-linux-amd64-0.9.2-ea9f0d2.tar.gz
+    wget https://github.com/piplabs/story-geth/releases/download/v0.10.0/geth-linux-amd64
     [ ! -d "$HOME/go/bin" ] && mkdir -p $HOME/go/bin
     if ! grep -q "$HOME/go/bin" $HOME/.bash_profile; then
         echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
     fi
-    sudo cp geth-linux-amd64-0.9.2-ea9f0d2/geth $HOME/go/bin/story-geth
-    rm -rf geth-linux-amd64-0.9.2-ea9f0d2
+    chmod +x geth-linux-amd64
+    mv $HOME/geth-linux-amd64 $HOME/go/bin/story-geth
     source $HOME/.bash_profile
     story-geth version
     check_status "Story-Geth installed"
@@ -56,11 +54,9 @@ install_story_geth() {
 # Download and install Story binary
 install_story_binary() {
     print_color "blue" "Downloading and installing Story binary..."
-    wget https://story-geth-binaries.s3.us-west-1.amazonaws.com/story-public/story-linux-amd64-0.9.13-b4c7db1.tar.gz
-    tar -xzvf story-linux-amd64-0.9.13-b4c7db1.tar.gz
-    rm story-linux-amd64-0.9.13-b4c7db1.tar.gz
-    cp $HOME/story-linux-amd64-0.9.13-b4c7db1/story $HOME/go/bin
-    rm -rf $HOME/story-linux-amd64-0.9.13-b4c7db1
+    wget https://github.com/piplabs/story/releases/download/v0.12.0/story-linux-amd64
+    chmod +x story-linux-amd64
+    sudo cp $HOME/story-linux-amd64 $HOME/go/bin/story
     source $HOME/.bash_profile
     story version
     check_status "Story binary installed"
@@ -70,8 +66,8 @@ install_story_binary() {
 init_iliad_node() {
     local moniker=$1
     print_color "blue" "Initializing Iliad node..."
-    story init --network iliad --moniker "$moniker"
-    check_status "Iliad node initialized"
+    story init --network odyssey --moniker "$moniker"
+    check_status "Odyssey node initialized"
 }
 
 # Create service files
@@ -85,7 +81,7 @@ After=network.target
 
 [Service]
 User=root
-ExecStart=/root/go/bin/story-geth --iliad --syncmode full
+ExecStart=/root/go/bin/story-geth --odyssey --syncmode full
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=4096
